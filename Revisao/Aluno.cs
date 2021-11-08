625c4f240b5cd2d3e6a0f459208df2e2ced91146
Stephany Alves <stephany@pixemotion.com.br>
2021-11-08 10:33:56 -0300

    Initial commit

---
+++ Revisao/Aluno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Revisao
+{
+    public struct Aluno
+    {
+        public string Nome { get; set; }
+        public decimal Nota { get; set; }
+    }
+}
---
+++ Revisao/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //le a informação digitada
+            string UsuarioOpcao = ObterOpcao();
+
+            //ENQUANTO a opção dogitada for diferente de x ele vai trazer as opções 
+            while (UsuarioOpcao.ToUpper() != "X")
+            {
+                switch (UsuarioOpcao)
+                {
+                    case "1":
+                        //FAZ: adicionar aluno
+                        break;
+                    case "2":
+                        //FAZ: lista aluno
+                        break;
+                    case "3":
+                        //FAZ: Calcula média geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                UsuarioOpcao = ObterOpcao();
+            }
+
+        }
+
+        //metodos sempre ficam fora do static void main**
+        private static string ObterOpcao()
+        {
+            string UsuarioOpcao;
+            //Pega as informações que o usuario digitar
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Insere novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            //lê NOVAMENTE a opção digitada
+            UsuarioOpcao = Console.ReadLine();
+            return UsuarioOpcao;
+        }
+    }
+}
